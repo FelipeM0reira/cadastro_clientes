7bc7f771a8d0be9940e1a71ef731a7fea958fc8c
Felipe Moreira <104744113+FelipeM0reira@users.noreply.github.com>
2024-11-13 22:20:30 -0300

    Update CidadeController.php

--- app/Http/Controllers/CidadeController.php
+++ app/Http/Controllers/CidadeController.php
@@ -9,7 +9,7 @@ use Exception;
 class CidadeController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource. aa
      */
     public function index()
     {
